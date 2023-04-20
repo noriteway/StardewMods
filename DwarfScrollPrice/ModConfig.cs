19b933d3366e288a2924e3e850a90d88c35e629f
noriteway <131356289+noriteway@users.noreply.github.com>
2023-04-20 03:24:43 -0700

    DwarfScrollPrice 1.0.0

---
+++ DwarfScrollPrice/ModConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DwarfScrollPrice
+{
+    public sealed class ModConfig
+    {
+        public int price1 { get; set; } = 110;
+        public int price2 { get; set; } = 120;
+        public int price3 { get; set; } = 130;
+        public int price4 { get; set; } = 140;
+    }
+}
